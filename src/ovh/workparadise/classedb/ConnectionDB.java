01c459b420feade9463aa861ffde60c7cdfbe2cd
louis <louis_laurent@aol.fr>
2018-04-22 23:31:54 +0200

    Avancer sur le back reste le problème du clear à réglerer + tester

--- src/ovh/workparadise/classedb/ConnectionDB.java
+++ src/ovh/workparadise/classedb/ConnectionDB.java
@@ -3,8 +3,11 @@ package ovh.workparadise.classedb;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
+
 
 public class ConnectionDB {
 	private String _url;
@@ -13,6 +16,7 @@ public class ConnectionDB {
 	private String _request;
 	private Connection _connection = null;
 	private PreparedStatement _statement = null;
+	private ResultSet _result = null;
 	
 	public ConnectionDB(String dbName) {
 		this._url = "jdbc:mysql://localhost:3306/" + dbName;
@@ -67,30 +71,38 @@ public class ConnectionDB {
 		return this._statement;
 	}
 	
+	public ResultSet getResult() {
+		return this._result;
+	}
+	
 	// Setter
 	
 	public void setUrl(String url) {
 		this._url = url;
 	}
 	
-	public void set_user(String user) {
+	public void setUser(String user) {
 		this._user = user;
 	}
 	
-	public void set_pwd(String pwd) {
+	public void setPwd(String pwd) {
 		this._pwd = pwd;
 	}
 
-	public void set_request(String request) {
+	public void setRequest(String request) {
 		this._request = request;
 	}
 
-	public void set_connection(Connection connection) {
+	public void setConnection(Connection connection) {
 		this._connection = connection;
 	}
 
-	public void set_statement(PreparedStatement statement) {
+	public void setStatement(PreparedStatement statement) {
 		this._statement = statement;
+	}
+	
+	public void setResult(ResultSet result) {
+		this._result = result;
 	}
 	
 	// Méthode
@@ -116,18 +128,122 @@ public class ConnectionDB {
 		}
 	}
 	
-	/*public void completeRequest(ArrayList<Parameter> list) {
+	public void completeRequest(@SuppressWarnings("rawtypes") ArrayList<Parameter> list) {
 		for(int i = 0; i < list.size(); i++) {
-			if(list.get(i).getType() == "int") {
-				this._statement.setInt(i, ((int) list.get(i)).intValue());
-				
-			}else if(list.get(i).getType() == "String") {
-				this._statement.setString(i, ((String) list.get(i)));
-				
-			}else if(list.get(i).getType() == "float") {
-				this._statement.setFloat(i, ((float) list.get(i)).floatValue());
+			try {
+				if(list.get(i).getType() == "int") {
+					this._statement.setInt(i, (int)list.get(i).get());
+					
+				}else if(list.get(i).getType() == "String") {
+					this._statement.setString(i, (String)list.get(i).get());
+					
+				}else if(list.get(i).getType() == "float") {
+					this._statement.setFloat(i, (float)list.get(i).get());
+					
+				}else if(list.get(i).getType() == "double") {
+					this._statement.setDouble(i, (double)list.get(i).get());
+					
+				}else if(list.get(i).getType() == "long") {
+					this._statement.setLong(i, (long)list.get(i).get());
+					
+				}else if(list.get(i).getType() == "Date") {
+					this._statement.setDate(i, (java.sql.Date)list.get(i).get());
+					
+				}else {
+					System.out.println("Erreur type de données inconnu");
+					return;
+				}
+			
+			}catch(SQLException e) {
+				e.printStackTrace();
 				
 			}
 		}
-	}*/
+	}
+	
+	public ResultSet returnExecute() {
+		try {
+			this._result = this._statement.executeQuery();
+		
+		}catch(SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return this._result;
+		
+	}
+	
+	public void noReturnExecute() {
+		try {
+			this._statement.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	public void clearSql() {
+		this.clearConnection();
+		this.clearRequest();
+		this.clearResult();
+		
+	}
+	
+	public void clearRequest() {
+		try {
+			if(this._statement != null) {
+				this._statement.close();
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}		
+	}
+	
+	public void clearConnection() {
+		try {
+			if(this._connection != null) {
+				this._connection.close();
+				
+			}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}		
+	}
+	
+	public void clearResult() {
+		try {
+			if(this._result != null) {
+				this._result.close();
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}		
+	}
+	
+	public void updateData(String sql, @SuppressWarnings("rawtypes") ArrayList<Parameter> list) {
+		this.driverConnection();
+		this.prepareRequest(sql);
+		this.completeRequest(list);
+		this.noReturnExecute();
+		this.clearSql();
+		
+	}
+	
+	public ResultSet getData(String sql, @SuppressWarnings("rawtypes") ArrayList<Parameter> list) {
+		this.driverConnection();
+		this.prepareRequest(sql);
+		this.completeRequest(list);
+		return this.returnExecute();
+	}
 }
